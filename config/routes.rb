1147ab44e50a9fe4aa37d53f33f0bb60663045a3
Dimon <litvindimalitvin01@gmail.com>
2023-03-21 21:57:15 +0300

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'session/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get  'login'  => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
   # The priority is based upon order of creation: first created -> highest priority.
